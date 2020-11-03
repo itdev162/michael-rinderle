e724c488b077a8084122446ed2e5add4c94979a1
michael <michael@sofdigital.net>
2020-11-02 22:45:27 -0600

    added several endpoints to PostController.cs

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,13 +1,12 @@
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Mvc;
-
 using Application.Posts;
-
 using Domain;
-
+using Persistence;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -16,12 +15,68 @@ namespace API.Controllers
     public class PostsController : ControllerBase
     {
         private readonly IMediator mediator;
+        private readonly DataContext context;
 
-        public PostsController(IMediator mediator) => this.mediator = mediator;
-
-        public async Task<ActionResult<List<Post>>> List()
+        public PostsController(IMediator mediator, DataContext context)
         {
-            return await this.mediator.Send(new List.Query());
+            this.mediator = mediator;
+            this.context = context;
         }
+
+        // public async Task<ActionResult<List<Post>>> List()
+        // {
+        //     return await this.mediator.Send(new List.Query());
+        // }
+
+        /// <summary>
+        /// GET api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
+        [HttpGet]
+        public ActionResult<List<Post>> Get()
+        {
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post 
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            this.context.Posts.Add(post);
+            var success = this.context.SaveChanges() > 0;
+            if(success)
+            {
+                return post;
+            }
+            throw new Exception("Error creating post");
+        }
+
+
+
+
+
     }
 }
